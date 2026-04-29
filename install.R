8bb9974959803eab25a4984f7c8b161f3f842339
muhamhamza123 <126794203+muhamhamza123@users.noreply.github.com>
2026-04-29 15:20:47 +0300

    Remove IRkernel installation from install.R
    Removed installation of IRkernel package and its spec.

--- install.R
+++ install.R
@@ -1,7 +1,5 @@
-install.packages("IRkernel")
 install.packages("tidyverse")
 install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
 install.packages("leaflet")
-IRkernel::installspec()
